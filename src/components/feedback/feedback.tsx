880ba4b0fb9a20f95fcc24ef4cf313186641ccf2
Anastasia <nastyakopylova1912.02@gmail.com>
2023-03-28 11:34:27 +0400

    newСhangesFeedback

--- src/components/feedback/feedback.tsx
+++ src/components/feedback/feedback.tsx
@@ -6,48 +6,35 @@ import Rows from '../rows/rows';
 import Container from '../container/container';
 import styles from './scss/style.module.scss'
 import { ICoincidence } from '../../interfaces/coincidens.interface';
+// import styles from './scss/delivery.module.scss';
+import Button from '../button/button';
 
-enum types {
-  full= 'Полное совпадение',
-  partial = 'Частичное совпадение',
-  another = 'Интересные предложения'
-}
-
-const FeedBack = observer(() => {
-  const { userAuthorized } = useMemo(() => new UserStore(), []);
-  const coincidencesStore  = useMemo(() => new CoincidencesStore(userAuthorized?.id), []);
-
-  useEffect(()=>{
-    if(userAuthorized?.id){
-    coincidencesStore.setCoencidences(userAuthorized.id)
-    }
-  }
-  ,[userAuthorized]);
+const FeedBack = () => {
+  
   return (
-    <Container>
-    <div  className ={styles.feedback}>
-      {coincidencesStore.coincidences && (
-        <div className={styles.feedback__block}>
-          {Object.keys(coincidencesStore.coincidences).map(type=>{
-            return (
-            <>
-            <h3>{types[type]}</h3>
-            <Rows rows={(coincidencesStore.coincidences![type]).map((row: ICoincidence)=>{
-              return {
-                userName: row.user.userName,
-                city: row.address.addrCity,
-                rating: row.user.rating
-              }
-            })} />
-            </>)
-          })}
-          
-        </div>
-        
-      )}
-    </div>
-    </Container>
+    <>
+      <Container>
+        <>
+          <div className={styles.feedback__wrap}>
+            <div className={styles.feedback__bblock}>
+                <div className={styles.feedback__bblock1}></div>
+            </div>
+            <div className={styles.feedback__bblock}>
+                <div className={styles.feedback__bblock2}></div>
+            </div>
+          </div>
+
+          <div className={styles.feedback__box1}>
+            <input className={styles.feedback__input1} />
+            <button className={styles.feedback__button1}>Отправить</button>
+          </div>
+        </>
+      </Container>
+    </>
   );
-});
+};
 
 export default FeedBack;
+
+
+// https://codesandbox.io/s/wizardly-night-ul6hr?file=/src/index.js:0-60
